9353b3a7f68e24a1e7b48c16ff7659043ac204a6
satkalra <satyam_kalra@yahoo.co.in>
2023-04-19 09:07:48 -0400

    Supabase, query and users
    Added supabase DB
    Changed queryset for todos filtered by user
    Added new app users

--- challenge2/urls.py
+++ challenge2/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views  
 
 urlpatterns =[
-    path('todos/', views.ToDosProtected.as_view(), name ='todos'),
+    path('todos/<int:id>', views.ToDosProtected.as_view(), name ='todos'),
     path('todo_create/', views.ToDoCreateProtected.as_view(), name='todo_create'),
     path('todo_create', views.ToDoUpdateDeleteProtected.as_view(), name='view_todo'),
     path('todo_update/<uuid:pk>/', views.ToDoUpdateDeleteProtected.as_view(), name='todo_update'),
